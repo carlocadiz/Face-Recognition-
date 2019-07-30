2f00a4f9cb6120d042d2fe07cc5c8e6ac1e9e2c7
Carlo Cadiz <carlocadiz71@gmail.com>
2019-07-30 09:27:48 -0400

    added Clarifai api functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: '8cebe6e687e843c5a0338128940e53b7'
+});
 
 const particlesOptions ={
-    particles: {
-      number: {
-        value:100,
-        density: {
-          enable: true,
-          value_area: 1000
-        }
+      "particles": {
+          "number": {
+              "value": 50
+          },
+          "size": {
+              "value": 3
+          }
+      },
+      "interactivity": {
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              }
+          }
       }
-    }
-}
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={ particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+    this.setState({input : event.target.value})
+  }
+
+  onButtonSubmit = () =>{
+    //console.log('click');
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+    .then(
+    function(response) {
+      // do something with response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
   );
+  }
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={ particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}
+                        onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
