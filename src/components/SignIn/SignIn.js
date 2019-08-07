ed58c315e23639bab4c0c67b89cd2d8288571660
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-06 22:40:15 -0400

    linked register component to server

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -35,7 +35,7 @@ class SignIn extends React.Component {
   }
 
   render() {
-    const { onRouteChange } = this.props;
+    //const { onRouteChange } = this.props;
     return(
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -67,7 +67,7 @@ class SignIn extends React.Component {
                 value="Sign in"/>
             </div>
             <div className="lh-copy mt3">
-              <p onClick={()=> onRouteChange('register')} className="f6 link dim black pointer">Register</p>
+              <p onClick={()=> this.props.onRouteChange('register')} className="f6 link dim black pointer">Register</p>
             </div>
           </div>
         </main>
