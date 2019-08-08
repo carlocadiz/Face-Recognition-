3158df3cdc786e19fe8c45642dfc14b7b668456e
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-08 09:33:52 -0400

    fixed validation and security issues

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 
-const app = new Clarifai.App({
- apiKey: '8cebe6e687e843c5a0338128940e53b7'
-});
+
+
 
 const particlesOptions ={
       "particles": {
@@ -88,10 +86,14 @@ class App extends Component {
   onButtonSubmit = () => {
     //console.log('click');
     this.setState({imageUrl: this.state.input});
-    app.models
-    .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+        })
+        .then(response => response.json())
     .then(response => {
       if (response) {
         fetch('http://localhost:3000/image', {
