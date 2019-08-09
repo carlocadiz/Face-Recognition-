01d2b52a34f133cead0d35dff11072c03d06cac3
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-08 23:21:57 -0400

    changed to heroku http

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
   onButtonSubmit = () => {
     //console.log('click');
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://secure-eyrie-34912.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
         .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://secure-eyrie-34912.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
